1355d77a136e8ba51ac786c32d227b486a7168dc
bojan laptop <bojan.damjanovski1@gmail.com>
2025-02-02 21:16:50 +0100

    Individual Matches made

--- match.py
+++ match.py
@@ -13,29 +13,43 @@ class Match:
             Results.Won_X: (1, -1),
             Results.Won_0: (-1, 1),
             Results.Tie: (0, 0),
-            Results.Failed_X: (-3, 1),
-            Results.Failed_0: (1, -3),
+            Results.Failed_X: (-3, 0.5),
+            Results.Failed_0: (0.5, -3),
         }
+        self.result = (0, 0)
 
     def predict_to_point(self, OutputLayer: list):
         i = OutputLayer.index(max(OutputLayer))
         return (i // 3, i % 3)
 
-    def move(self, player: Agent):
+    def move(self, player: Agent, print: bool = False):
         InputLayer = self.grid.getX() if self.grid.turn == "X" else self.grid.getY()
         pos = self.predict_to_point(player.predict(InputLayer=InputLayer))
-        print(pos)
+        if print:
+            print(pos)
         if self.grid.place(pos):  # True if pos is a legal placement
             self.grid.check()
 
-    def play(self):
-        print(self.grid)
+    def play(self, turn="X", print: bool = False):
+        self.grid.__init__()
+        self.grid.turn = turn
+        if print:
+            print(self.grid)
         while not self.grid.done():
             self.move(self.player1 if self.grid.turn == "X" else self.player2)
-            print(self.grid)
+            if print:
+                print(self.grid)
         return self.resultMap[self.grid.result]
+
+    def getResult(self):
+        self.result = tuple(map(sum, zip(self.result, self.play())))
+        self.result = tuple(map(sum, zip(self.result, self.play("0"))))
+        self.result = tuple(map(sum, zip(self.result, self.play())))
+        return self.result
 
 
 if __name__ == "__main__":
-    test = Match()
-    test.play()
+    # print(Match().getResult())
+    agents = [(Agent(), Agent()) for i in range(250)]
+    results = [Match(P1, P2).getResult() for P1, P2 in agents]
+    print(max(results))
